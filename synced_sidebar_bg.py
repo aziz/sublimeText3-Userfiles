37217508afbae7d6ef4e43d50af276214ece7de4
Allen Bargi <allen.bargi@gmail.com>
2014-05-25 02:16:40 +1000

    cache previous result to speed things up (if it's the same theme don't do anything)

--- synced_sidebar_bg.py
+++ synced_sidebar_bg.py
@@ -2,11 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # later:
-# cache previous result to speed things up (if it's the same theme don't do anything)
 # enable or disable with a custom setting
 # darken/lighten sidebar by a percentage
 # color highlighter issue
-
 
 import sublime, sublime_plugin
 import codecs, json
@@ -14,62 +12,75 @@ from os import path
 from plistlib import readPlist
 
 
-
 class SidebarMatchColorScheme(sublime_plugin.EventListener):
 
     def on_activated_async(self, view):
-        if True:
-            path_packages = sublime.packages_path()
-            scheme_file = view.settings().get('color_scheme')
-            color_scheme = path.normpath(scheme_file)
-            plist_path = path_packages + color_scheme.replace('Packages', '')
-            if plist_path.endswith("Widgets.stTheme"):
-                return
-            # print(plist_path)
-            plist_file = readPlist(plist_path)
-            color_settings = plist_file["settings"][0]["settings"]
-            # print(color_settings)
+        global cache
+        scheme_file = view.settings().get('color_scheme')
+        if scheme_file == cache.get('color_scheme'):
+            return
+        color_scheme = path.normpath(scheme_file)
+        path_packages = sublime.packages_path()
+        plist_path = path_packages + color_scheme.replace('Packages', '')
+        if plist_path.endswith("Widgets.stTheme"):
+            return
+        # print(plist_path)
+        plist_file = readPlist(plist_path)
+        color_settings = plist_file["settings"][0]["settings"]
+        # print(color_settings)
 
-            bg = color_settings.get("background", '#FFFFFF')
-            fg = color_settings.get("foreground", '#000000')
+        bg = color_settings.get("background", '#FFFFFF')
+        fg = color_settings.get("foreground", '#000000')
 
-            _NUMERALS = '0123456789abcdefABCDEF'
-            _HEXDEC = {v: int(v, 16) for v in (x+y for x in _NUMERALS for y in _NUMERALS)}
+        cache = {
+            "bg": bg,
+            "fg": fg,
+            "color_scheme": scheme_file
+        }
+        # print("Updating sidebar BG to", cache)
 
-            def rgb(triplet):
-                return _HEXDEC[triplet[0:2]], _HEXDEC[triplet[2:4]], _HEXDEC[triplet[4:6]]
+        _NUMERALS = '0123456789abcdefABCDEF'
+        _HEXDEC = {v: int(v, 16) for v in (x+y for x in _NUMERALS for y in _NUMERALS)}
 
-            def is_light(triplet):
-                r, g, b = _HEXDEC[triplet[0:2]], _HEXDEC[triplet[2:4]], _HEXDEC[triplet[4:6]]
-                yiq = ((r*299)+(g*587)+(b*114))/1000;
-                return yiq >= 128
+        def rgb(triplet):
+            return _HEXDEC[triplet[0:2]], _HEXDEC[triplet[2:4]], _HEXDEC[triplet[4:6]]
 
-            def label_color(triplet):
-                if is_light(triplet):
-                    return [30, 30, 30]
-                else:
-                    return [220, 220, 220]
+        def is_light(triplet):
+            r, g, b = _HEXDEC[triplet[0:2]], _HEXDEC[triplet[2:4]], _HEXDEC[triplet[4:6]]
+            yiq = ((r*299)+(g*587)+(b*114))/1000;
+            return yiq >= 128
 
-            bgc = bg.lstrip('#')
+        def label_color(triplet):
+            if is_light(triplet):
+                return [30, 30, 30]
+            else:
+                return [220, 220, 220]
 
-            template = [
-                {
-                    "class": "sidebar_tree",
-                    "layer0.tint": rgb(bgc),
-                    "layer0.opacity": 1,
-                    "dark_content": not is_light(bgc)
-                },
-                {
-                    "class": "sidebar_label",
-                    "color": label_color(bgc),
-                },
-                {
-                    "class": "sidebar_heading",
-                    "shadow_offset": [0, 0]
-                }
-            ]
+        bgc = bg.lstrip('#')
 
-            json_str = json.dumps(template, sort_keys=True, indent=4, separators=(',', ': ')).encode('raw_unicode_escape')
-            new_theme_file_path = path_packages + "/User/" + view.settings().get('theme')
-            with codecs.open(new_theme_file_path, 'w', 'utf-8') as f:
-                f.write(json_str.decode())
+        template = [
+            {
+                "class": "sidebar_tree",
+                "layer0.tint": rgb(bgc),
+                "layer0.opacity": 1,
+                "dark_content": not is_light(bgc)
+            },
+            {
+                "class": "sidebar_label",
+                "color": label_color(bgc),
+            },
+            {
+                "class": "sidebar_heading",
+                "shadow_offset": [0, 0]
+            }
+        ]
+
+        json_str = json.dumps(template, sort_keys=True, indent=4, separators=(',', ': ')).encode('raw_unicode_escape')
+        new_theme_file_path = path_packages + "/User/" + view.settings().get('theme')
+        with codecs.open(new_theme_file_path, 'w', 'utf-8') as f:
+            f.write(json_str.decode())
+
+
+def plugin_loaded():
+    global cache
+    cache = {}
