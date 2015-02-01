76c2ec27cad38b9cc3c2dedcedd7a08f40d05b18
Allen Bargi <allen.bargi@gmail.com>
2015-02-01 18:41:47 +1100

    added back sublimeREPL

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sublime, sublime_plugin
+
+# class TestTestPlugin(sublime_plugin.TextCommand):
+#     def run(self, edit, event):
+#         print(event)
+
+#     def want_event(self):
+#         return True
