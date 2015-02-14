4a5796dab7e885440bec276c156f8a40666338c7
Allen Bargi <allen.bargi@gmail.com>
2015-02-14 23:03:42 +1100

    added download.py

---
+++ download.py
@@ -0,0 +1,157 @@
+from urllib import parse, request
+from threading import Thread
+from collections import namedtuple
+import sublime, sublime_plugin
+import time, queue, json, re
+from os import listdir
+from os.path import join, dirname
+
+callbacks = vars(vars()['sublime_plugin'])
+
+
+def get_packages_uris():
+    PC_URI = "https://packagecontrol.io/packages/"
+    pc = sublime.load_settings("Package Control.sublime-settings")
+    installed_packages = pc.get("installed_packages")
+    urls = [PC_URI + parse.quote(p) + ".json" for p in installed_packages]
+    return urls
+
+
+def get_url(u):
+    try:
+        json_str = request.urlopen(u).read().decode("utf-8")
+        j = json.loads(json_str)
+        last_modified = time.strptime(j["last_modified"], "%Y-%m-%dT%H:%M:%SZ")
+        return j["name"], last_modified
+    except:
+        return "error", u
+
+
+def pprinttable(rows):
+    """
+    Usage:
+        from collections import namedtuple
+        Row = namedtuple('Row',['first','second','third'])
+        data = Row(1,2,3)
+        pprinttable([data, data, data, data])
+    """
+    result = []
+    if len(rows) > 1:
+        headers = rows[0]._fields
+        lens = []
+        for i in range(len(rows[0])):
+            lens.append(len(max([x[i] for x in rows] + [headers[i]], key=lambda x: len(str(x)))))
+        formats = []
+        hformats = []
+        for i in range(len(rows[0])):
+            if isinstance(rows[0][i], int):
+                formats.append("%%%dd" % lens[i])
+            else:
+                formats.append("%%-%ds" % lens[i])
+            hformats.append("%%-%ds" % lens[i])
+        pattern = " | ".join(formats)
+        hpattern = " | ".join(hformats)
+        separator = "-+-".join(['-' * n for n in lens])
+        result.append(hpattern % tuple(headers))
+        result.append(separator)
+        for line in rows:
+            result.append(pattern % tuple(line))
+    elif len(rows) == 1:
+        row = rows[0]
+        hwidth = len(max(row._fields, key=lambda x: len(x)))
+        for i in range(len(row)):
+            result.append("%*s = %s" % (hwidth, row._fields[i], row[i]))
+    return "\n".join(result)
+
+
+class PackStatsTableCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        def_path = join(dirname(sublime.executable_path()), 'Packages')
+        default = set([re.sub(r'\.sublime-package', '', p) for p in listdir(def_path)])
+        user = set(listdir(sublime.packages_path()))
+        pc = set([re.sub(r'\.sublime-package', '', p) for p in listdir(sublime.installed_packages_path())])
+        disabled = set(sublime.load_settings('Preferences.sublime-settings').get('ignored_packages', []))
+        ignored = set(["User", "bz2", "0_package_control_loader"])
+
+        enabled_def = default - disabled
+        disabled_def = default - enabled_def
+        pc_total = (pc | (user - default)) - ignored
+        enabled_pc = pc_total - disabled
+        disabled_pc = pc_total - enabled_pc
+        total = (pc | user | disabled | default) - ignored
+        enabled = total - disabled
+
+        Row = namedtuple('Row', ['Type', 'Total', 'Disabled', 'Enabled'])
+        row1 = Row("Built-in", len(default), len(disabled_def), len(enabled_def))
+        row2 = Row("Package Control", len(pc_total), len(disabled_pc), len(enabled_pc))
+        row3 = Row("Total", len(total), len(disabled), len(enabled))
+        results = pprinttable([row1, row2, row3])
+
+        out = self.view.window().get_output_panel("stats")
+        self.view.window().run_command("show_panel", {"panel": "output.stats"})
+        out.insert(edit, out.size(), results)
+
+
+class PackStatsLastupdateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        concurrent = 200
+        self.data = []
+        self.q = queue.Queue(concurrent * 2)
+        self.Row = namedtuple('Row', ['Package', 'Last_Update'])
+
+        for _ in range(concurrent):
+            t = Thread(target=self.doWork)
+            t.daemon = True
+            t.start()
+
+        urls = get_packages_uris()
+        print(len(urls))
+        for u in urls:
+            self.q.put(u)
+        self.q.join()
+        self.data.sort(key=lambda x: x[1], reverse=True)
+        rows = []
+        for d in self.data:
+            rows.append(self.Row(d[0], time.strftime("%Y-%m-%d", d[1])))
+        results = pprinttable(rows)
+        out = self.view.window().get_output_panel("stats")
+        self.view.window().run_command("show_panel", {"panel": "output.stats"})
+        out.insert(edit, out.size(), results)
+
+    def doWork(self):
+        while True:
+            url = self.q.get()
+            name, last_modified = get_url(url)
+            if name == "error":
+                print("Error: " + last_modified)
+            else:
+                self.data.append((name, last_modified))
+            self.q.task_done()
+
+
+class PackStatsEventsReport(sublime_plugin.TextCommand):
+    def run(self, edit):
+        events = ["on_selection_modified", "on_selection_modified_async", "on_modified", "on_modified_async", "on_activated", "on_deactivated", "on_activated_async", "on_deactivated_async", "on_text_command", "on_post_text_command", "on_window_command", "on_post_window_command", "on_query_completions", "on_query_context", "on_pre_close", "on_pre_save", "on_pre_save_async", "on_close", "on_load", "on_load_async", "on_new", "on_new_async", "on_post_save", "on_post_save_async", "on_clone", "on_clone_async"]
+        report = []
+        empty_events = []
+        report.append("All the events registered by your installed and enabled packages:")
+        report.append("NOTE:")
+        report.append("  The events on the top are the most expensive ones. please consider")
+        report.append("  disabling the plugins if you are not using them.")
+        report.append("-" * 70)
+        for e in events:
+            event = callbacks['all_callbacks'][e]
+            if len(event) > 0:
+                report.append(e)
+            else:
+                empty_events.append(e)
+
+            for obj in event:
+                if obj.__class__.__name__ == "SublimeEventLogger":
+                    continue
+                report.append(u"  - {0}.{1}".format(obj.__class__.__module__, obj.__class__.__name__))
+        empty = "\n" + "-" * 70 + "\nEmpty Events:\n" + "\n".join(empty_events)
+        x = "\n".join(report) + empty
+        out = sublime.active_window().get_output_panel("event_reports")
+        self.view.window().run_command("show_panel", {"panel": "output.event_reports"})
+        out.insert(edit, out.size(), x)
--- test.py
+++ test.py
@@ -1,165 +1,29 @@
 import sublime, sublime_plugin
-import time
-from urllib import request
-from urllib.parse import quote
-from os import path, listdir
-from os.path import join, dirname
-import re
-from collections import namedtuple
 
-# ------------------------------------------------------------------------------
-
-callbacks = vars(vars()['sublime_plugin'])
-
-
-def log_event(event_name):
-    global callbacks
-    event_cbs = callbacks['all_callbacks'][event_name]
-    print(u"➤ `{0}` fired:".format(event_name))
-    for obj in event_cbs:
-        if obj.__class__.__name__ == "SublimeEventLogger":
-            continue
-        print(u"- {0}.{1}".format(obj.__class__.__module__, obj.__class__.__name__))
-
-
-class SublimeEventReport(sublime_plugin.TextCommand):
-    def run(self, edit):
-        events = ["on_selection_modified", "on_selection_modified_async", "on_modified", "on_modified_async", "on_activated", "on_deactivated", "on_activated_async", "on_deactivated_async", "on_text_command", "on_post_text_command", "on_window_command", "on_post_window_command", "on_query_completions", "on_query_context", "on_pre_close", "on_pre_save", "on_pre_save_async", "on_close", "on_load", "on_load_async", "on_new", "on_new_async", "on_post_save", "on_post_save_async", "on_clone", "on_clone_async"]
-        report = []
-        empty_events = []
-        report.append("All the events registered by your installed and enabled packages:")
-        report.append("NOTE:")
-        report.append("  The events on the top are the most expensive ones. please consider")
-        report.append("  disabling the plugins if you are not using them.")
-        report.append("-"*70)
-        for e in events:
-            event = callbacks['all_callbacks'][e]
-            if len(event) > 0:
-                report.append(e)
-            else:
-                empty_events.append(e)
-
-            for obj in event:
-                if obj.__class__.__name__ == "SublimeEventLogger":
-                    continue
-                report.append(u"  - {0}.{1}".format(obj.__class__.__module__, obj.__class__.__name__))
-        empty = "\n" + "-" * 70 + "\nEmpty Events:\n" + "\n".join(empty_events)
-        x = "\n".join(report) + empty
-        out = sublime.active_window().get_output_panel("event_reports")
-        self.view.window().run_command("show_panel", {"panel": "output.event_reports"})
-        out.insert(edit, out.size(), x)
-
-
+# callbacks = vars(vars()['sublime_plugin'])
+#
+# def log_event(event_name):
+#     global callbacks
+#     event_cbs = callbacks['all_callbacks'][event_name]
+#     print(u"➤ `{0}` fired:".format(event_name))
+#     for obj in event_cbs:
+#         if obj.__class__.__name__ == "SublimeEventLogger":
+#             continue
+#         print(u"- {0}.{1}".format(obj.__class__.__module__, obj.__class__.__name__))
+#
+#
 # class SublimeEventLogger(sublime_plugin.EventListener):
 #     def on_selection_modified(self, view):
 #         log_event('on_selection_modified')
-
+#
 #     def on_selection_modified_async(self, view):
 #         log_event('on_selection_modified_async')
-
+#
 #     def on_modified(self, view):
 #         log_event('on_modified')
-
+#
 #     def on_modified_async(self, view):
 #         log_event('on_modified_async')
-
-# ------------------------------------------------------------------------------
-
-
-def pprinttable(rows):
-    """
-    Usage:
-        from collections import namedtuple
-        Row = namedtuple('Row',['first','second','third'])
-        data = Row(1,2,3)
-        pprinttable([data, data, data, data])
-    """
-    result = []
-    if len(rows) > 1:
-        headers = rows[0]._fields
-        lens = []
-        for i in range(len(rows[0])):
-            lens.append(len(max([x[i] for x in rows] + [headers[i]], key=lambda x: len(str(x)))))
-        formats = []
-        hformats = []
-        for i in range(len(rows[0])):
-            if isinstance(rows[0][i], int):
-                formats.append("%%%dd" % lens[i])
-            else:
-                formats.append("%%-%ds" % lens[i])
-            hformats.append("%%-%ds" % lens[i])
-        pattern = " | ".join(formats)
-        hpattern = " | ".join(hformats)
-        separator = "-+-".join(['-' * n for n in lens])
-        result.append(hpattern % tuple(headers))
-        result.append(separator)
-        for line in rows:
-            result.append(pattern % tuple(line))
-    elif len(rows) == 1:
-        row = rows[0]
-        hwidth = len(max(row._fields, key=lambda x: len(x)))
-        for i in range(len(row)):
-            result.append("%*s = %s" % (hwidth, row._fields[i], row[i]))
-    return "\n".join(result)
-
-
-class PackStatsCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        default = set([re.sub(r'\.sublime-package', '', p) for p in listdir(join(dirname(sublime.executable_path()), 'Packages'))])
-        user = set(listdir(sublime.packages_path()))
-        pc = set([re.sub(r'\.sublime-package', '', p) for p in listdir(sublime.installed_packages_path())])
-        disabled = set(sublime.load_settings('Preferences.sublime-settings').get('ignored_packages', []))
-        ignored = set(["User", "bz2", "0_package_control_loader"])
-
-        enabled_def = default - disabled
-        disabled_def = default - enabled_def
-        pc_total = (pc | (user - default)) - ignored
-        enabled_pc = pc_total - disabled
-        disabled_pc = pc_total - enabled_pc
-        total = (pc | user | disabled | default) - ignored
-        enabled = total - disabled
-
-        Row = namedtuple('Row', ['Type', 'Total', 'Disabled', 'Enabled'])
-        row1 = Row("Built-in", len(default), len(disabled_def), len(enabled_def))
-        row2 = Row("Package Control", len(pc_total), len(disabled_pc), len(enabled_pc))
-        row3 = Row("Total", len(total), len(disabled), len(enabled))
-        results = pprinttable([row1, row2, row3])
-
-        out = self.view.window().get_output_panel("pack_stats")
-        self.view.window().run_command("show_panel", {"panel": "output.pack_stats"})
-        out.insert(edit, out.size(), results)
-
-
-# ------------------------------------------------------------------------------
-
-
-def get_packages_uris():
-    PC_URI = "https://packagecontrol.io/packages/"
-    pc = sublime.load_settings("Package Control.sublime-settings")
-    installed_packages = pc.get("installed_packages")
-    urls = [PC_URI + quote(p) + ".json" for p in installed_packages]
-    return urls
-
-
-class PackagesStatsCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        urls = get_packages_uris()
-        data = []
-        Row = namedtuple('Row', ['Package', 'Last_Update'])
-        for u in urls[-50:]:
-            json_str = request.urlopen(u).read().decode("utf-8")
-            json = sublime.decode_value(json_str)
-            last_modified = time.strptime(json["last_modified"], "%Y-%m-%dT%H:%M:%SZ")
-            data.append((json["name"], last_modified))
-        data.sort(key=lambda x: x[1], reverse=True)
-        rows = []
-        for d in data:
-            rows.append(Row(d[0], time.strftime("%Y-%m-%d", d[1])))
-        results = pprinttable(rows)
-        out = self.view.window().get_output_panel("pack_stats")
-        self.view.window().run_command("show_panel", {"panel": "output.pack_stats"})
-        out.insert(edit, out.size(), results)
-
 
 # ------------------------------------------------------------------------------
 # TODO: remember selection and move back to selection after insert
