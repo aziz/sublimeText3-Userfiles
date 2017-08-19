7070a4b25d087ea0b927c36b16a3f610189b25fd
Allen Bargi <allen.bargi@gmail.com>
2017-08-19 20:02:48 +0200

    cleanup

--- sidebarEnhancementFU.py
+++
@@ -1,8 +0,0 @@
-from SideBarEnhancements.SideBar import *
-
-class SideBarDonateCommand(sublime_plugin.WindowCommand):
-    def is_visible(self):
-        return False
-
-    def is_enabled(self):
-        return False
